2ff0aa2e1e28b5a47a3af33814346f491c9bc381
Ernedin Zajko <ezajko@root.ba>
2025-11-30 18:22:04 +0100

    fix: import CSS/SCSS in Storybook preview
    Added imports to .storybook/preview.js:
    - Bootstrap 5 CSS
    - Bootstrap Icons
    - Custom SCSS (_ez-custom.scss with all ez- components)
    
    Now styles will be applied to all Storybook stories.
    
    Author: Ernedin Zajko <ezajko@root.ba>

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,10 @@
+// Import Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+// Import Bootstrap Icons
+import 'bootstrap-icons/font/bootstrap-icons.css';
+// Import custom SCSS (ez- components)
+import '../src/scss/_ez-custom.scss';
+
 export default {
     parameters: {
         actions: { argTypesRegex: '^on[A-Z].*' },
