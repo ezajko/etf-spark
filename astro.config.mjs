6844cdbe951dffc86cfdee954c40b2622095b4ac
Ernedin Zajko <ezajko@root.ba>
2025-12-06 14:51:50 +0100

    ci: switch to pnpm for github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://etf-unsa.github.io',
-    base: '/www-ng',
+    site: 'https://ezajko.github.io',
+    base: '/etf-spark',
     integrations: [sitemap()],
     vite: {
         css: {
