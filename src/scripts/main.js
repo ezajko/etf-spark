81edc719f8e14a1f276834ab2781384a2ba62286
Ernedin Zajko <ezajko@root.ba>
2025-12-14 10:18:47 +0100

    Fix: Expose bootstrap globally in main.js to support inline component scripts

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,6 @@
-// ETF UNSA Design System - Main JS Entry Point
-// Imports Bootstrap's JS machinery
+import * as bootstrap from 'bootstrap';
+window.bootstrap = bootstrap;
 
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './sidebar-menu.js';
 
 console.log('ETF UNSA Design System Loaded');
