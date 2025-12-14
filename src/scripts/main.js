8a4007e3397e8b4c5d93d0b9aef4dd7fab24fed1
Ernedin Zajko <ezajko@root.ba>
2025-12-14 10:27:45 +0100

    Refactor: Extract HeaderMobile and CalendarWidget scripts to main JS bundle

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,5 +2,7 @@ import * as bootstrap from 'bootstrap';
 window.bootstrap = bootstrap;
 
 import './sidebar-menu.js';
+import './mobile-menu.js';
+import './calendar-widget.js';
 
 console.log('ETF UNSA Design System Loaded');
