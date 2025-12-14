1a8cc9cb88c051be7312db37e183dc74c297ce00
Ernedin Zajko <ezajko@root.ba>
2025-12-14 08:49:01 +0100

    Refactor: Major source reorganization. Moved app source to src/site, structured components and SCSS overrides.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
     site: 'https://ezajko.github.io',
     base: '/etf-spark',
+    srcDir: './src/site',
     integrations: [sitemap()],
     vite: {
         css: {
